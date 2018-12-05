d19136fd473fd432c2ae22656aeae6830c955838
Dan DuLeone <dan@duleone.com>
2018-12-05 18:34:37 -0500

    Adding Dan DuLeone to repository

---
+++ dd/index.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+  console.log('Dan DuLeone was here.');
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+const dan_duleone = require('./dd');
+
+const team = [
+  dan_duleone
+]
+
+team.forEach((incubee) => {
+  incubee();
+});
